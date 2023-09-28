1f4f33e9754fc6569b9d3b1391ddcfe6a41a436d
lpieri <pieri.louisse@gmail.com>
2023-09-28 18:47:41 +0200

    feat: add get shipping methods with args

--- method/client.go
+++ method/client.go
@@ -1,6 +1,7 @@
 package method
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/lpieri/sendcloud"
@@ -16,6 +17,40 @@ func New(apiKey string, apiSecret string) *Client {
 		apiKey:    apiKey,
 		apiSecret: apiSecret,
 	}
+}
+
+func (c *Client) GetMethodsWithArgs(fromPostalCode string, toPostalCode string, isReturn bool, senderAddress string, servicePointID int64) ([]*sendcloud.Method, error) {
+	smr := sendcloud.MethodListResponseContainer{}
+
+	URL := "/api/v2/shipping_methods?to_country=FR"
+	if fromPostalCode != "" {
+		URL = fmt.Sprint(URL, "&from_postal_code=", fromPostalCode)
+	}
+	if toPostalCode != "" {
+		URL = fmt.Sprint(URL, "&to_postal_code=", fromPostalCode)
+	}
+	if isReturn {
+		URL = fmt.Sprint(URL, "&is_return=", isReturn)
+	}
+	if senderAddress != "" {
+		URL = fmt.Sprint(URL, "&sender_address=", senderAddress)
+	}
+	if servicePointID > 0 {
+		URL = fmt.Sprint(URL, "&service_point_id=", servicePointID)
+	}
+
+	err := sendcloud.Request(
+		"GET",
+		URL,
+		nil,
+		c.apiKey,
+		c.apiSecret,
+		&smr,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return smr.GetResponse().([]*sendcloud.Method), nil
 }
 
 // Get all shipment methods
